f7d6f297e00d8914ceb5eec3e02a4a056a216885
Otor John Stephen <johnogenyi4real@gmail.com>
2021-03-15 00:28:24 +0100

    created the scroll to top icon and added styles and script for it's functionality

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 
 function scrollHeader(){
     const nav = document.getElementById('header')
-    //when the scroll is greater than 200 view port height, add the scroll-header class
+    //when the scroll is greater than 100 view port height, add the scroll-header class
     if(this.scrollY >= 100) nav.classList.add('scroll-header');
     else
     nav.classList.remove('scroll-header');
 }
 
 window.addEventListener('scroll', scrollHeader)
+
+
+
+//SHOW SCROLL TOP
+
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+    //when the scroll is higher than 560 view port height, add the show-scroll class
+    if(this.scrollY >= 560) scrollTop.classList.add('scroll-top');
+    else
+    scrollTop.classList.remove('scroll-top');
+}
+window.addEventListener('scroll', scrollTop)
